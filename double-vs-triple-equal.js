79ddf4d9519d70694e65c0d54c04755a9bed4139
Iftekhar Hasan <iftekharhasanrifat@gmail.com>
2021-02-02 21:49:28 +0000

    double vs triple equal

---
+++ double-vs-triple-equal.js
@@ -0,0 +1,10 @@
+// == only checks the value and converts the value and checks
+// === checks the value and type
+const first = 2;
+const second = "2";
+if (first === second) {
+    console.log("condition true");
+}
+else {
+    console.log("condition false");
+}
